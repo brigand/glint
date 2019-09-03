63b139c1514c09fdcbe7c5cc8962539cf50e886e
Frankie Bagnardi <f.bagnardi@gmail.com>
2019-09-02 17:16:48 -0700

    feat(git): adds git module with commit method

---
+++ src/git.rs
@@ -0,0 +1,59 @@
+use std::env::current_dir;
+use std::ffi::OsStr;
+use std::fmt;
+use std::io;
+use std::path::PathBuf;
+use std::process::{Command, Stdio};
+
+pub struct Git {
+  cwd: PathBuf,
+}
+
+pub enum GitError {
+  NotGitRepo,
+  Io(io::Error),
+}
+
+impl Git {
+  pub fn from_cwd() -> Result<Self, GitError> {
+    let cwd = current_dir().map_err(GitError::Io)?;
+
+    let git_dir = cwd.join(".git");
+
+    if !git_dir.is_dir() {
+      return Err(GitError::NotGitRepo);
+    }
+
+    Ok(Git { cwd })
+  }
+
+  pub fn commit<I>(&self, message: &str, other_args: impl IntoIterator<Item = I>) -> Command
+  where
+    I: AsRef<OsStr>,
+  {
+    let mut command = Command::new("git");
+
+    // Setup
+    command.current_dir(&self.cwd);
+    command.stdin(Stdio::null());
+
+    // Args
+    command.arg("commit");
+    command.arg("-m");
+    command.arg(message);
+    for arg in other_args {
+      command.arg(arg);
+    }
+
+    command
+  }
+}
+
+impl fmt::Display for GitError {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    match self {
+      GitError::NotGitRepo => write!(f, "This directory is not a git repository."),
+      GitError::Io(err) => write!(f, "Internal I/O error: {}", err),
+    }
+  }
+}
