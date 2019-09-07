ae6f76febf4343a9cbe2ec9e85ab7d10181c0cf8
Frankie Bagnardi <f.bagnardi@gmail.com>
2019-09-07 16:09:42 -0700

    feat(files_prompt): impl prompt for selecting files to commit

--- src/prompt.rs
+++ src/prompt.rs
@@ -1,7 +1,9 @@
+mod files_prompt;
 mod message_prompt;
 mod scope_prompt;
 mod type_prompt;
 
+pub use files_prompt::{FilesPrompt, FilesPromptResult};
 pub use message_prompt::{MessagePrompt, MessagePromptResult};
 pub use scope_prompt::{ScopePrompt, ScopePromptResult};
 pub use type_prompt::{TypePrompt, TypePromptResult};
---
+++ src/prompt/files_prompt.rs
@@ -0,0 +1,126 @@
+use crate::color::reset_display;
+use crate::git::GitStatus;
+use crate::Config;
+use crate::TermBuffer;
+use crossterm::{self as ct, style, InputEvent, KeyEvent};
+
+#[derive(Debug)]
+pub struct FilesPrompt<'a> {
+  config: &'a mut Config,
+  checked: Vec<bool>,
+  selected_index: u16,
+  options: GitStatus,
+}
+
+pub enum FilesPromptResult {
+  Files(Vec<String>),
+  Escape,
+  Terminate,
+}
+
+impl<'a> FilesPrompt<'a> {
+  pub fn new(config: &'a mut Config, options: GitStatus) -> Self {
+    FilesPrompt {
+      config,
+      checked: (0..options.len()).map(|_| false).collect(),
+      selected_index: 0,
+      options,
+    }
+  }
+
+  pub fn run(mut self) -> FilesPromptResult {
+    let mut buffer = TermBuffer::new();
+
+    let input = crossterm::input();
+    let mut sync_stdin = input.read_sync();
+
+    let figlet = self
+      .config
+      .get_figlet()
+      .expect("Ensure figlet_file points to a valid file, or remove it.");
+
+    let mut first_iteration = true;
+    loop {
+      let event = if first_iteration {
+        first_iteration = false;
+        None
+      } else {
+        sync_stdin.next()
+      };
+
+      match event {
+        Some(InputEvent::Keyboard(KeyEvent::Ctrl('c'))) => {
+          return FilesPromptResult::Terminate;
+        }
+        Some(InputEvent::Keyboard(KeyEvent::Char(' '))) => {
+          let index = self.selected_index as usize;
+          self.checked[index] = !self.checked[index];
+        }
+        Some(InputEvent::Keyboard(KeyEvent::Char('\n'))) => {
+          let selected = self
+            .options
+            .iter()
+            .enumerate()
+            .filter_map(|(i, file)| Some(file).filter(|_| self.checked[i]))
+            .map(Into::into)
+            .collect();
+          return FilesPromptResult::Files(selected);
+        }
+
+        Some(InputEvent::Keyboard(KeyEvent::Esc)) => {
+          return FilesPromptResult::Escape;
+        }
+        Some(InputEvent::Keyboard(KeyEvent::Up)) => {
+          self.selected_index = match self.selected_index {
+            0 => 0,
+            x => x.saturating_sub(1),
+          };
+        }
+        Some(InputEvent::Keyboard(KeyEvent::Down)) => {
+          let total = self.options.len() as u16;
+
+          self.selected_index += 1;
+          if self.selected_index >= total {
+            self.selected_index = total.saturating_sub(1);
+          }
+        }
+        _ => {}
+      };
+
+      let mut header = figlet.create_vec();
+      figlet.write_to_buf_color("<glint>", header.as_mut_slice(), |s| {
+        ct::style(s).with(ct::Color::Magenta).to_string()
+      });
+
+      for line in header {
+        buffer.push_line(line);
+      }
+
+      let prompt_pre = "Toggle files to commit (with <space>):";
+      let underscores = "-".repeat(prompt_pre.len());
+      buffer.push_line("");
+      buffer.push_line(prompt_pre);
+      buffer.push_line(format!("{}{}", underscores, reset_display()));
+
+      let y_offset = buffer.lines() + self.selected_index;
+
+      let selected_color = style("").with(ct::Color::Blue).to_string();
+      for (i, item) in self.options.iter().enumerate() {
+        let color = if i as u16 == self.selected_index {
+          &selected_color as &str
+        } else {
+          ""
+        };
+
+        let prefix = if self.checked[i] { "☑" } else { "□" };
+
+        let line = format!("{}{} {}{}", color, prefix, item.file(), reset_display());
+        buffer.push_line(line);
+      }
+
+      buffer.set_next_cursor((0, y_offset));
+      buffer.render_frame();
+      buffer.flush();
+    }
+  }
+}
